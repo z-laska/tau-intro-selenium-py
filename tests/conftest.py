f0f3284058b172f142732b4529dba02aafa4c94e
zuzanna.laska <zuna150@gmail.com>
2023-08-08 16:16:26 +0200

    multiple browsers, running tests in pararell

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,44 @@ This module contains shared fixtures.
 
 import pytest
 from selenium import webdriver
+
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+import json
 
 
 @pytest.fixture()
-def browser():
+def config(scope='session'):
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+    assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+    assert isinstance(config['implicit_wait'], int)
+    assert config['implicit_wait'] > 0
+
+    return config
+
+
+@pytest.fixture()
+def browser(config):
 
     # Initialize the ChromeDriver instance
-    b = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    if config['browser'] == 'Firefox':
+        b = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+    elif config['browser'] == 'Chrome':
+        b = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    elif config['browser'] == 'Headless Chrome':  # test fail in this mode! need to work on that
+        opts = webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        b = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=opts)
+    else:
+        raise Exception(f'Browser "{config["browser"]} is not supported')
 
     # Wait up to 10 seconds for the elements to appear
-    b.implicitly_wait(10)
+    b.implicitly_wait(config['implicit_wait'])
 
     # Return the WebDriver instance for the setup
     yield b
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,30 +1,30 @@
 """
 Tests covering DuckDuckGo searches.
 """
-
+import pytest
 
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['zebra', 'python', 'arctic fox'])
+def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "zebra"
 
     # Given the DuckDuckGo home page is displayed
     search_page.load()
 
     # When the user searched for "zebra"
-    search_page.search(PHRASE)
+    search_page.search(phrase)
 
-    # Then the search result title contains "zebra"
-    assert PHRASE in result_page.title()
-
-    # And the search result query is "zebra"
-    assert PHRASE == result_page.search_input_value()
+    # Then the search result query is "zebra"
+    assert phrase == result_page.search_input_value()
 
     # And the search result links pertain to "zebra"
     titles = result_page.result_link_titles()
-    matches = [t for t in titles if PHRASE.lower() in t.lower()]
+    matches = [t for t in titles if phrase.lower() in t.lower()]
     assert len(matches) > 0
+
+    # And the search result title contains "zebra"
+    assert phrase in result_page.title()
